641bb1fa7edb0d9e8ea690a37e8bc3764191a9e2
Riley Bathurst <riley@rileybathurst.com>
2019-05-29 09:26:08 -0700

    keep squaw true update

--- single-post-keep-squaw-true.php
+++ single-post-keep-squaw-true.php
@@ -6,52 +6,61 @@ if (have_posts()) {
 	get_template_part( 'template-parts/post/specific-top' ); ?>
 
 	<div class="article-content-specific">
-		<div class="grid-x grid-padding-x">
+		<hr class="mega-hr">
 
-			<div class="small-12 medium-6 cell">
-				<p>
+		<div class="specific-grid">
+			<h3 class="subtitle">Video</h3>
+
+			<div class="continental east-medium">
+				<p>Working on the post product for the Movie to Keep Squaw True was my first experience with a full length documentary. Working under editor and co-dorector Scott Gaffney I worked extensivley on the color, graphics and smaller edits, trailers and extra pieces involved in the movie.</p>
+			</div>
+
+			<div class="continental west-medium gr-1-m">
+				<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/300523913" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+			</div>
+		</div>
+
+		<hr class="mega-hr">
+
+		<div class="specific-grid">
+			<div class="continental west-medium">
+				<p>The locals Shreddit Edit was an action piece accompanying The Movie to Keep Squaw True featuring some of the best local atheletes in the region Sierra Watch are trying to protect and was played alongside the movie as a hype piece at film festivals.</p>
+			</div>
+
+			<div class="continental east-medium">
+				<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/327133013" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+			</div>
+
+		</div>
+
+		<hr class="mega-hr">
+
+		<div class="specific-grid">
+			<h3 class="subtitle">Photography</h3>
+
+			<div class="continental">
+				<p>Keep Squaw True</p>
+			</div>
+
+			<div class="continental west-medium mw-18em">
 					<picture class="lozad-picture" data-iesrc="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_094-small.jpg">
 						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_094-large.jpg" alt="keep squaw true clothing shoot" media="(min-width: 64em)">
 						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_094-medium.jpg" alt="keep squaw true clothing shoot" media="(min-width: 40em)">
 						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_094-small.jpg" alt="keep squaw true clothing shoot">
 						<noscript><img src="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_094-small.jpg" alt="keep squaw true clothing shoot"></noscript>
 					</picture>
-				</p>
 			</div>
 
-			<div class="small-12 medium-6 cell">
-				<p>
-					<picture class="lozad-picture" data-iesrc="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_135-small.jpg">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_135-large.jpg" alt="keep squaw true clothing shoot" media="(min-width: 64em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_135-medium.jpg" alt="keep squaw true clothing shoot" media="(min-width: 40em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_135-small.jpg" alt="keep squaw true clothing shoot">
-						<noscript><img src="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_Clothing_V2_135-small.jpg" alt="keep squaw true clothing shoot"></noscript>
-					</picture>
-				</p>
-			</div>
+			<div class="east-medium"></div> <!-- white space -->
 
-			 <div class="small-12 medium-6 cell">
-				<p>
-					<picture class="lozad-picture" data-iesrc="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-large.jpg" alt="keep squaw true clothing shoot" media="(min-width: 64em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-medium.jpg" alt="keep squaw true clothing shoot" media="(min-width: 40em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg" alt="keep squaw true clothing shoot">
-						<noscript><img src="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg" alt="keep squaw true clothing shoot"></noscript>
-					</picture>
-				</p>
+			<div class="continental east-medium mw-18em">
+				<picture class="lozad-picture" data-iesrc="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg">
+					<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-1.jpg" alt="keep squaw true clothing shoot" media="(min-width: 64em)">
+					<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-medium.jpg" alt="keep squaw true clothing shoot" media="(min-width: 40em)">
+					<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg" alt="keep squaw true clothing shoot">
+					<noscript><img src="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0315-small.jpg" alt="keep squaw true clothing shoot"></noscript>
+				</picture>
 			</div>
-
-			<div class="small-12 medium-6 cell">
-				<p>
-					<picture class="lozad-picture" data-iesrc="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0051-small.jpg">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0051-large-2.jpg" alt="keep squaw true clothing shoot" media="(min-width: 64em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0051-medium.jpg" alt="keep squaw true clothing shoot" media="(min-width: 40em)">
-						<source srcset="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0051-small.jpg" alt="keep squaw true clothing shoot">
-						<noscript><img src="<?php echo content_url(); ?>/uploads/2018/05/Keep_Squaw_True_apparel_0051-small.jpg" alt="keep squaw true clothing shoot"></noscript>
-					</picture>
-				</p>
-			</div>
-
 
 		</div>
 	</div> <!-- .article-content-specific -->
