09f7e127b36c40f115ffa510b8538c13b3413cb6
Riley Bathurst <riley@rileybathurst.com>
2019-06-26 11:15:22 -0700

    split the InterssectionObservers theres still some problem with overlapping variables

--- footer.php
+++ footer.php
@@ -1,86 +1,87 @@
 <footer class="site-footer">
 
-		<?php wp_nav_menu( array(
-			'theme_location'    => 'primary',
-			'menu_class'        => 'footer_menu text-center',
-		) ); ?>
+    <?php wp_nav_menu( array(
+      'theme_location'    => 'primary',
+      'menu_class'        => 'footer_menu text-center',
+    ) ); ?>
 
-	<div class="footer_info">
-		<div class="footer_copyright">
-			<?php if(get_option('copyright') == 'on'){echo '&copy;'; } ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-			<?php $established = get_option('established'); ?>
-			<?php echo get_option('established'); ?><?php if(date("Y")!=$established){ echo ' - '.date("Y"); } else { echo date("Y"); } ?>
+  <div class="footer_info">
+    <div class="footer_copyright">
+      <?php if(get_option('copyright') == 'on'){echo '&copy;'; } ?>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+      <?php $established = get_option('established'); ?>
+      <?php echo get_option('established'); ?><?php if(date("Y")!=$established){ echo ' - '.date("Y"); } else { echo date("Y"); } ?>
 
-		</div>
-		<div class="footer_nav">
-			<?php wp_nav_menu( array(
-				'theme_location' => 'secondary',
-				'menu_id'        => 'bottom-menu',
-			) ); ?>
-		</div>
-		<div class="footer_contact">
-			<a href="mailto:<?php echo bloginfo ('admin_email'); ?>"><?php echo bloginfo ('admin_email'); ?></a>
-			<!-- add a contact number to the theme -->
-			<?php if (get_option('contact_phone')) {
-				$unspaced_phone = str_replace(' ', '', get_option('contact_phone')); ?>
-				<a href="tel:<?php echo $unspaced_phone; ?>"><?php echo get_option('contact_phone'); ?></a>
-			<?php } ?>
-		</div>
-	</div>
+    </div>
+    <div class="footer_nav">
+      <?php wp_nav_menu( array(
+        'theme_location' => 'secondary',
+        'menu_id'        => 'bottom-menu',
+      ) ); ?>
+    </div>
+    <div class="footer_contact">
+      <a href="mailto:<?php echo bloginfo ('admin_email'); ?>"><?php echo bloginfo ('admin_email'); ?></a>
+      <!-- add a contact number to the theme -->
+      <?php if (get_option('contact_phone')) {
+        $unspaced_phone = str_replace(' ', '', get_option('contact_phone')); ?>
+        <a href="tel:<?php echo $unspaced_phone; ?>"><?php echo get_option('contact_phone'); ?></a>
+      <?php } ?>
+    </div>
+  </div>
 
 </footer>
 
 <script>
-	var numSteps = 20.0;
+  // some variables have been used above in the laptop screen flipper
+  var numSteps2 = 20.0;
 
-	var spineElement;
-	var prevRatio = 0.0;
-	var mT = "ratioem";
+  var spineElement;
+  var prevRatio2 = 0.0;
+  var mT = "ratio2em";
 
-	// Set things up.
+  // Set things up.
 
-	window.addEventListener("load", function(event) {
-		spineElement = document.querySelectorAll(".spine-border");
+  window.addEventListener("load", function(event) {
+    spineElement = document.querySelectorAll(".spine-border");
 
-		createObserver();
-	}, false);
+    createObserver2();
+  }, false);
 
-	function createObserver() {
-		var observer;
+  function createObserver2() {
+    var observer2;
 
-		var options = {
-			root: null,
-			rootMargin: "0px",
-			threshold: buildThresholdList()
-		};
+    var options2 = {
+      root: null,
+      rootMargin: "0px",
+      threshold: buildThresholdList2()
+    };
 
-		observer = new IntersectionObserver(handleIntersect, options);
+    observer2 = new IntersectionObserver(handleIntersect2, options2);
 
-		spineElement.forEach(spineElements => {
-			observer.observe(spineElements);
-		});
-	}
+    spineElement.forEach(spineElements => {
+      observer2.observe(spineElements);
+    });
+  }
 
-	function buildThresholdList() {
-		var thresholds = [];
-		var numSteps = 100;
+  function buildThresholdList2() {
+    var thresholds2 = [];
+    var numSteps2 = 100;
 
-		for (var i=1.0; i<=numSteps; i++) {
-			var ratio = i/numSteps;
-			thresholds.push(ratio);
-		}
+    for (var j=1.0; j<=numSteps2; j++) {
+      var ratio2 = j/numSteps2;
+      thresholds2.push(ratio2);
+    }
 
-		thresholds.push(0);
-		return thresholds;
-	}
+    thresholds2.push(0);
+    return thresholds2;
+  }
 
-	function handleIntersect(entries, observer) {
-		entries.forEach(function(entry) {
-			entry.target.style.marginTop = mT.replace("ratio", (entry.intersectionRatio*5));
-			prevRatio = entry.intersectionRatio;
-		});
-	}
+   function handleIntersect2(entries, observer2) {
+    entries.forEach(function(entry) {
+      entry.target.style.marginTop = mT.replace("ratio2", (entry.intersectionRatio*5));
+      prevRatio2 = entry.intersectionRatio;
+    });
+  }
 
 </script>
 
@@ -91,5 +92,5 @@
 
 <?php wp_footer(); ?>
 
-	</body>
+  </body>
 </html>
--- template-parts/header/priest-image.php
+++ template-parts/header/priest-image.php
@@ -1,66 +1,66 @@
 <figure class="custom-header-media">
 
-	<img src="<?php echo content_url(); ?>/uploads/2019/05/priest-macbook-white-2019-screen.png" alt="priest sheetmetal website design and development" class="folding" style="transform-origin: bottom; transition: 3s ease-in-out;">
-	<img src="<?php echo content_url(); ?>/uploads/2019/05/priest-macbook-white-2019-base.png" alt="priest sheetmetal website design and development" class="base">
+  <img src="<?php echo content_url(); ?>/uploads/2019/05/priest-macbook-white-2019-screen.png" alt="priest sheetmetal website design and development" class="folding" style="transform-origin: bottom; transition: 3s ease-in-out;">
+  <img src="<?php echo content_url(); ?>/uploads/2019/05/priest-macbook-white-2019-base.png" alt="priest sheetmetal website design and development" class="base">
 </figure>
 
-	<script>
-		if ('IntersectionObserver' in window &&
-			'IntersectionObserverEntry' in window &&
-			'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+  <script>
+    if ('IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
 
-				let flatten = document.querySelector('.folding');
-				console.log(flatten.classList);
-				// ["cool", "new", "shades", value: "cool new shades"]
-				console.log(flatten.classList[1]); // new
-				flatten.classList.add('flatten');
+        let flatten = document.querySelector('.folding');
+        console.log(flatten.classList);
+        // ["cool", "new", "shades", value: "cool new shades"]
+        console.log(flatten.classList[1]); // new
+        flatten.classList.add('flatten');
 
-				var numSteps = 20.0;
+        var numSteps = 20.0;
 
-				var boxElement;
-				var prevRatio = 0.0;
-				var wkT = "rotateX(ratiodeg)";
+        var boxElement;
+        var prevRatio = 0.0;
+        var wkT = "rotateX(ratiodeg)";
 
-				// Set things up.
+        // Set things up.
 
-				window.addEventListener("load", function(event) {
-				  boxElement = document.querySelector(".folding");
+        window.addEventListener("load", function(event) {
+          boxElement = document.querySelector(".folding");
 
-				  createObserver();
-				}, false);
+          createObserver();
+        }, false);
 
-				function createObserver() {
-				  var observer;
+        function createObserver() {
+          var observer;
 
-				  var options = {
-					root: null,
-					rootMargin: "0px",
-					threshold: buildThresholdList()
-				  };
+          var options = {
+          root: null,
+          rootMargin: "0px",
+          threshold: buildThresholdList()
+          };
 
-				  observer = new IntersectionObserver(handleIntersect, options);
-				  observer.observe(boxElement);
-				}
+          observer = new IntersectionObserver(handleIntersect, options);
+          observer.observe(boxElement);
+        }
 
-				function buildThresholdList() {
-				  var thresholds = [];
-				  var numSteps = 100;
+        function buildThresholdList() {
+          var thresholds = [];
+          var numSteps = 100;
 
-				  for (var i=1.0; i<=numSteps; i++) {
-					var ratio = i/numSteps;
-					thresholds.push(ratio);
-				  }
+          for (var i=1.0; i<=numSteps; i++) {
+          var ratio = i/numSteps;
+          thresholds.push(ratio);
+          }
 
-				  thresholds.push(0);
-				  return thresholds;
-				}
+          thresholds.push(0);
+          return thresholds;
+        }
 
-				function handleIntersect(entries, observer) {
-				  entries.forEach(function(entry) {
-					  entry.target.style.webkitTransform = wkT.replace("ratio", (90-(entry.intersectionRatio*90)));
-					prevRatio = entry.intersectionRatio;
-				  });
-				}
-			}
+        function handleIntersect(entries, observer) {
+          entries.forEach(function(entry) {
+            entry.target.style.webkitTransform = wkT.replace("ratio", (90-(entry.intersectionRatio*90)));
+          prevRatio = entry.intersectionRatio;
+          });
+        }
+      }
 
-	</script>
+  </script>
