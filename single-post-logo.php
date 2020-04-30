c32bafa5a5f538fe3bd4c51dcb7520095f25e92b
Riley <riley@rileybathurst.com>
2020-04-30 10:50:54 -0700

    logo redirect

--- single-post-logo.php
+++ single-post-logo.php
@@ -3,7 +3,7 @@
 Template Name: Redirect to logos category
 */
 
-header('Location: https://rileybathurst.com/category/design-2/logo-2/');
+header('Location: https://rileybathurst.com/category/clicks/branding/');
 exit();
 
 ?>
