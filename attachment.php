b79369d0c89f5b3e3f60e7848e106b7c95453465
Riley Bathurst <riley@rileybathurst.com>
2021-09-27 11:02:00 -0700

    breadcrumb fix

--- attachment.php
+++ attachment.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 
-<h1>meow</h1>
+<!-- <h1>meow</h1> -->
 
 <div class="background-dirty">
 	<div class="site-main" role="main">
--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -5,13 +5,18 @@ function get_breadcrumb() {
 	echo '<a href="'.home_url().'" rel="nofollow">Home</a>&emsp;&#187;&emsp;';
 	if (is_category()) {
 		echo single_cat_title('');
+
+	// single
+	// this is picking up attachments
 	} elseif (is_single()) {
 		if (isset($_GET['c'])) { // if I have come from a category page keep it short
 			$incoming = $_GET['c'];
 			echo $incoming;
 			echo '&emsp;&#187;&emsp;';
 		} else {
-			echo the_category( '&emsp;&#187;&emsp;' )."&emsp;&#187;&emsp;";
+			if (the_category() != '') {
+				echo the_category( '&emsp;&#187;&emsp;' )."&emsp;&#187;&emsp;";
+			}
 		}
 		echo the_title();
 	} elseif (is_page()) {
@@ -21,7 +26,10 @@ function get_breadcrumb() {
 		echo '"<em>';
 		echo the_search_query();
 		echo '</em>"';
-	} elseif (is_attachment()) {
+	} elseif(is_attachment()) {
+
+		echo "hey";
+
 		echo "Attachment";
 	} elseif (is_404()) {
 		echo "missing?";
